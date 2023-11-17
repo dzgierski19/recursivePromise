0a4ad55a29412d18b5c293d6e6849542ea939f50
dzgierski19 <dzgierskigit@gmail.com>
2023-11-17 23:20:08 +0100

    added solution

---
+++ src/app/solution1.ts
@@ -0,0 +1,69 @@
+import { PROMISEARRAY } from "./Global";
+
+// solution with resolving promises because of its resolving speed
+
+// promises
+
+const recursivePromise = <T>(
+  arrayOfPromises: (() => Promise<T>)[],
+  accumulator: T[] = []
+): Promise<T[]> => {
+  return Promise.race(
+    arrayOfPromises.map((element, index) => {
+      return element().then((value) => {
+        return { value, index };
+      });
+    })
+  )
+    .then((firstPromise) => {
+      accumulator.push(firstPromise.value);
+      arrayOfPromises.splice(firstPromise.index, 1);
+      return { arrayOfPromises, accumulator };
+    })
+    .then(() => {
+      if (arrayOfPromises.length === 0) {
+        return accumulator;
+      }
+      return recursivePromise(arrayOfPromises, accumulator);
+    })
+    .catch((error) => {
+      return [...accumulator, error];
+    });
+};
+
+// ASYNC/AWAIT
+
+const recursivePromiseAsyncAwait = async <T>(
+  arrayOfPromises: (() => Promise<T>)[],
+  accumulator: T[] = []
+): Promise<T[]> => {
+  try {
+    const race = await Promise.race(
+      arrayOfPromises.map(async (element, index) => {
+        const firstPromise = await element();
+        return { firstPromise, index };
+      })
+    );
+    accumulator.push(race.firstPromise);
+    arrayOfPromises.splice(race.index, 1);
+    if (arrayOfPromises.length === 0) {
+      return accumulator;
+    }
+    console.log(arrayOfPromises.length);
+    return recursivePromiseAsyncAwait(arrayOfPromises, accumulator);
+  } catch (error) {
+    return accumulator;
+  }
+};
+
+console.time("A");
+recursivePromiseAsyncAwait(PROMISEARRAY).then((value) => {
+  console.log(value);
+  console.timeEnd("A");
+});
+
+//type guard
+
+//1. napisac prosty serwer, ktory ma post
+// express
+// zalozyc wirtualna karte
---
+++ src/app/solution2.ts
@@ -0,0 +1,51 @@
+import { PROMISEARRAY } from "./Global";
+
+// solution with resolving promises because of its place in original array
+
+// promises
+
+const recursivePromiseB = <T>(
+  arrayOfPromises: (() => Promise<T>)[],
+  accumulator: T[] = []
+): Promise<T[]> => {
+  arrayOfPromises.reverse();
+  const lastItem = arrayOfPromises[arrayOfPromises.length - 1]();
+  return lastItem
+    .then((lastItem) => {
+      accumulator.push(lastItem);
+      arrayOfPromises.pop();
+      if (arrayOfPromises.length === 0) {
+        return accumulator;
+      }
+      return recursivePromiseB(arrayOfPromises.reverse(), accumulator);
+    })
+    .catch((error) => {
+      return accumulator;
+    });
+};
+
+// ASYNC/AWAIT
+
+const recursivePromiseAsyncAwaitB = async <T>(
+  arrayOfPromises: (() => Promise<T>)[],
+  accumulator: T[] = []
+): Promise<T[]> => {
+  try {
+    arrayOfPromises.reverse();
+    const lastItem = await arrayOfPromises[arrayOfPromises.length - 1]();
+    accumulator.push(lastItem);
+    arrayOfPromises.pop();
+    if (arrayOfPromises.length === 0) {
+      return accumulator;
+    }
+    return recursivePromiseAsyncAwaitB(arrayOfPromises.reverse(), accumulator);
+  } catch (error) {
+    return accumulator;
+  }
+};
+
+console.time("A");
+recursivePromiseAsyncAwaitB(PROMISEARRAY).then((value) => {
+  console.log(value);
+  console.timeEnd("A");
+});
